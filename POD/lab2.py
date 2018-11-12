66284ff840a99fb91d34e39e062957758c871892
Łukasz Dawydzik <lukasz.dawydzik@student.put.poznan.pl>
2018-11-12 12:18:12 +0100

    Delete lab2.py

--- POD/lab2.py
+++
@@ -1,9 +0,0 @@
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-
-key = get_random_bytes(16)
-cipher = AES.new(key, AES.MODE_EAX)
-ciphertext, tag = cipher.encrypt_and_digest(data)
-
-file_out = open("encrypted.bin", "wb")
-[ file_out.write(x) for x in (cipher.nonce, tag, ciphertext) ]
