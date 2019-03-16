75b9e384fdc6effc1be0784ccc1db26eeb4f6a50
Łukasz Dawydzik <lukasz.dawydzik@student.put.poznan.pl>
2019-03-16 23:47:33 +0100

    Update Multiplied_Cesar_cipher.py

--- POD/Multiplied_Cesar_cipher.py
+++ POD/Multiplied_Cesar_cipher.py
@@ -1,6 +1,6 @@
 import fractions
 
-alphabet_matrix={'A':0,'B':1,'C':2,'D':3,'E':4,'F':5,'G':6,'H':7,'I':8,'J':9,'K':10,
+alphabet_dict={'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5, 'G':6, 'H':7, 'I':8, 'J':9, 'K':10,
                  'L':11,'M':12,'N':13,'O':14,'P':15,'Q':16,'R':17,'S':18,'T':19,'U':20,
                  'V':21,'W':22,'X':23,'Y':24,'Z':25,' ':26,'.':27,',':28,'-':29,':':30}
 
@@ -9,30 +9,40 @@ alphabet_list=['A','B','C','D','E','F','G','H','I','J','K',
                'V','W','X','Y','Z',' ','.',',','-',':']
 
 def message_convertor_to_values(uppercase_raw_message):
-    """Method that converts message into integer values of letters, what simplify calculations"""
+    """Method that converts message into integer values of letters, what simplify calculations
+        Arguments:
+            :upercase_new_message: Message converted to uppercase"""
     message_into_values=[]
     for letter in uppercase_raw_message:
-        message_into_values.append(alphabet_matrix[letter])
-    return message_into_values
+        message_into_values.append(alphabet_dict[letter]) #Letters are converted into decimal values,
+                                                          #to count their value after encryption
+    return message_into_values #list of integers is returned
 
 def encryption(k, message_into_values):
-    """Cesar algorithm with multiplication"""
+    """Cesar algorithm with multiplication
+        Arguments:
+            :k: value of encryptor
+            :message_into_values: message written as list of integers"""
     encrypted_values = []
     i = 0
     for value in message_into_values:
-        encrypted_values.append((message_into_values[i] * k) % 31)
+        encrypted_values.append((message_into_values[i] * k) % 31)  #New letter's value is counted
         i = i + 1
-    return encrypted_values
+    return encrypted_values #list of integers is returned
 
-def translation_of_message(crypted_values):
-    """Translating numbers into message"""
+def translation_of_message(encrypted_values):
+    """Translating numbers into message
+        Arguments:
+          :encrypted_values:  message after encrypton and before changing into letters"""
     new_message = []
-    for value in crypted_values:
-        new_message.append(alphabet_list[value])
-    return new_message
+    for value in encrypted_values:
+        new_message.append(alphabet_list[value]) #Translating numbers into letters
+    return new_message #list of letters is returned
 
 def eulers_function(n):
-    """Method, that calculates Euler's function"""
+    """Method, that calculates Euler's function
+        Arguments:
+            :n: value of which Euler's function counted"""
     amount = 0
     for k in range(1, n + 1):
         if fractions.gcd(n, k) == 1:
@@ -40,42 +50,45 @@ def eulers_function(n):
     return amount
 
 def values_decryption(k,message_into_values):
+    """Cesar algorithm with multiplication
+         Arguments:
+             :k: value of decryptor
+             :message_into_values: message written as list of integers"""
     decrypted_values = []
     i = 0
     for value in message_into_values:
         decrypted_values.append((message_into_values[i] * (k ** (eulers_function(len(alphabet_list))-1))) % len(alphabet_list))
+        # New letter's value is counted
         i = i + 1
-    return decrypted_values
+    return decrypted_values #list of integers is returned
 
-#Input from console, output on screen version:
+
+#Ready to use functions:
+
 def encryption_input_from_console():
+    """Input from console, output on screen version"""
     raw_message=input("Please enter your message: ")
     uppercase_raw_message=raw_message.upper()
     message_into_values=message_convertor_to_values(uppercase_raw_message)
 
-    #print(message_into_values)
     k=int(input("Please enter the value of encryptor: "))
 
     encrypted_values=encryption(k,message_into_values)
-    #print(encrypted_values)
 
     encrypted_message=translation_of_message(encrypted_values)
     print("".join(encrypted_message))
 
-#Input from file, output to file version:
-
 def encryption_input_from_file():
-    input_file=open('lab1_pod_input.txt',mode='r')
+    """Input from file, output to file version"""
+    input_file=open('lab1_pod_input.txt',mode='r') #.txt file path
     raw_message=input_file.read()
 
     uppercase_raw_message = raw_message.upper()
     message_into_values = message_convertor_to_values(uppercase_raw_message)
 
-    #print(message_into_values)
-    k = 4
+    k = int(input("Please enter the value of decryptor: "))
 
     encrypted_values = encryption(k, message_into_values)
-    # print(encrypted_values)
 
     encrypted_message = translation_of_message(encrypted_values)
 
@@ -86,6 +99,7 @@ def encryption_input_from_file():
     print("Result stored in a file: lab1_pod_output.txt")
 
 def decryption_input_from_console():
+    """Decrypting message written by user"""
     raw_message = input("Please enter your message: ")
     uppercase_raw_message = raw_message.upper()
     message_into_values = message_convertor_to_values(uppercase_raw_message)
@@ -98,7 +112,8 @@ def decryption_input_from_console():
     print("".join(decrypted_message))
 
 def decryption_input_from_file():
-    input_file = open('lab1_pod_input_decryption.txt', mode='r')
+    """Decrypting message from .txt file"""
+    input_file = open('lab1_pod_input_decryption.txt', mode='r') #.txt file path
     raw_message = input_file.read()
     input_file.close()
     uppercase_raw_message = raw_message.upper()
@@ -114,27 +129,6 @@ def decryption_input_from_file():
     output_file.write(outpucik)
     output_file.close()
     print("Message saved in 'lab1_pod_output_decryption.txt")
-def encryption_for_labs():
-    input_file = open('lab1_pod_input.txt', mode='r')
-    raw_message = input_file.read()
-
-    uppercase_raw_message = raw_message.upper()
-    message_into_values = message_convertor_to_values(uppercase_raw_message)
 
 
-    encrypted_values = encryption(6, message_into_values)
-    # print(encrypted_values)
-
-    encrypted_message = translation_of_message(encrypted_values)
-
-    output_file = open('lab1_pod_output.txt', mode='w')
-    outpucik = "".join(encrypted_message)
-    output_file.write(outpucik)
-    output_file.close()
-    print("Result stored in a file: lab1_pod_output.txt")
-
-
-encryption_input_from_console()
-decryption_input_from_console()
-#encryption_input_from_file()
-#decryption_input_from_file()
+decryption_input_from_file()
